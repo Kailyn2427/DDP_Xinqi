3cf34ca86fd3c6e3b6d072179d6fbaf6c13ed405
Xinqi <83167597+Kailyn2427@users.noreply.github.com>
2024-01-28 18:04:24 +0000

    Add files via upload
    practice about string

---
+++ ExtracurricularLearning/4_Str.py
@@ -0,0 +1,22 @@
+a = "Hello"
+b = "Python"
+ 
+print("a + b OUTPUT：", a + b)
+print("a * 2 OUTPUT：", a * 2)
+print("a[1] OUTPUT：", a[1])
+print("a[1:4] OUTPUT：", a[1:4])
+ 
+if( "H" in a) :
+    print("H in variable a ")
+else :
+    print("H not in variable a ")
+ 
+if( "M" not in a) :
+    print("M not in variable a ")
+else :
+    print("M in variable a ")
+ 
+print (r'\n')
+print (R'\n')
+
+
